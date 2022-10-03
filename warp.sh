575cc3c8c77ef036349046fd4b8fef171ae0e2df
pig995 <70630613+pig995@users.noreply.github.com>
2022-10-03 09:04:28 -0700

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -84,26 +84,20 @@ WireGuard_Interface='wgcf'
 WireGuard_ConfPath="/etc/wireguard/${WireGuard_Interface}.conf"
 
 WireGuard_Interface_DNS_IPv4='8.8.8.8,8.8.4.4'
-WireGuard_Interface_DNS_IPv6='2001:4860:4860::8888,2001:4860:4860::8844'
-WireGuard_Interface_DNS_46="${WireGuard_Interface_DNS_IPv4},${WireGuard_Interface_DNS_IPv6}"
-WireGuard_Interface_DNS_64="${WireGuard_Interface_DNS_IPv6},${WireGuard_Interface_DNS_IPv4}"
+WireGuard_Interface_DNS_46="${WireGuard_Interface_DNS_IPv4}"
+WireGuard_Interface_DNS_64="${WireGuard_Interface_DNS_IPv4}"
 WireGuard_Interface_Rule_table='51888'
 WireGuard_Interface_Rule_fwmark='51888'
 WireGuard_Interface_MTU='1280'
 
 WireGuard_Peer_Endpoint_IP4='162.159.193.1'
-WireGuard_Peer_Endpoint_IP6='2606:4700:d0::a29f:c001'
 WireGuard_Peer_Endpoint_IPv4="${WireGuard_Peer_Endpoint_IP4}:2408"
-WireGuard_Peer_Endpoint_IPv6="[${WireGuard_Peer_Endpoint_IP6}]:2408"
 WireGuard_Peer_Endpoint_Domain='engage.cloudflareclient.com:2408'
 WireGuard_Peer_AllowedIPs_IPv4='0.0.0.0/0'
-WireGuard_Peer_AllowedIPs_IPv6='::/0'
-WireGuard_Peer_AllowedIPs_DualStack='0.0.0.0/0,::/0'
+WireGuard_Peer_AllowedIPs_DualStack='0.0.0.0/0'
 
 TestIPv4_1='1.0.0.1'
 TestIPv4_2='9.9.9.9'
-TestIPv6_1='2606:4700:4700::1001'
-TestIPv6_2='2620:fe::fe'
 CF_Trace_URL='https://www.cloudflare.com/cdn-cgi/trace'
 
 Get_System_Info() {
